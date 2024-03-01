3b63d8cfb8dd6839b0216ef72ed092530a0b2590
xianvy <xianvy0000@gmail.com>
2024-03-01 21:12:29 +0800

    Add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ProtectedRoute } from "./components/ProtectedRoutes";
 import PublicRoute from "./components/PublicRoutes";
 import { ROUTES } from "./constants/routes";
 import { populateInitialCategories } from "./firebase/inititalCategories";
+import ErrorBoundary from "./components/ErrorBoundary";
 function App() {
   console.log("App.");
   useEffect(() => {
@@ -14,31 +15,39 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          {ROUTES.map((route) => {
-            const RouteElement = (
-              <React.Suspense
-                fallback={
-                  <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
-                    Loading...
-                  </Box>
-                }
-              >
-                {route.element}
-              </React.Suspense>
-            );
-
-            if (route.isPrivate) {
-              return (
-                <Route key={route.path} path={route.path} element={<ProtectedRoute />}>
-                  <Route index element={RouteElement} />
-                </Route>
+        <ErrorBoundary>
+          <Routes>
+            {ROUTES.map((route) => {
+              const RouteElement = (
+                <React.Suspense
+                  fallback={
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      justifyContent="center"
+                      alignItems="center"
+                      height="100vh"
+                    >
+                      Loading...
+                    </Box>
+                  }
+                >
+                  {route.element}
+                </React.Suspense>
               );
-            } else {
-              return <Route key={route.path} path={route.path} element={<PublicRoute>{RouteElement}</PublicRoute>} />;
-            }
-          })}
-        </Routes>
+
+              if (route.isPrivate) {
+                return (
+                  <Route key={route.path} path={route.path} element={<ProtectedRoute />}>
+                    <Route index element={RouteElement} />
+                  </Route>
+                );
+              } else {
+                return <Route key={route.path} path={route.path} element={<PublicRoute>{RouteElement}</PublicRoute>} />;
+              }
+            })}
+          </Routes>
+        </ErrorBoundary>
       </Router>
     </div>
   );
