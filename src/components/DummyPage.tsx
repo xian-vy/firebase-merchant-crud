31d04820fa5e5e1df5a6b19ecd4a5188d927a26d
xianvy <xianvy0000@gmail.com>
2024-03-02 21:12:31 +0800

    remove unused components

--- src/components/DummyPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DummyPage = () => {
-  return (
-    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "90vh" }}>DummyPage</div>
-  );
-};
-
-export default DummyPage;
